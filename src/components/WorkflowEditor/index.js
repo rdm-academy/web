6a9a14144d6c73ab20166f4dc66c0596c41772ba
Byron Ruth <b@devel.io>
2017-07-10 19:23:12 -0400

    Add default workflow for new projects
    Signed-off-by: Byron Ruth <b@devel.io>

--- src/components/WorkflowEditor/index.js
+++ src/components/WorkflowEditor/index.js
@@ -18,6 +18,55 @@ import './style.css';
 window.jsyaml = YAML;
 
 
+const defaultWorkflow = `# Get started with your workflow!
+
+# There are four types of node you can declare in a
+# workflow: data, compute, manual, and finding.
+# Compute and manual nodes must receive at least
+# one data input and produce one output data.
+# A finding node must point to at last one data input.
+
+# Below is an example of a valid workflow to get you
+# started. The console below shows validation errors
+# as you type to keep you on track.
+
+patient-record:
+  type: data
+  title: Patient record
+
+chart-review-ai:
+  type: compute
+  title: Chart review AI
+  input:
+    - patient-record
+  output:
+    - ai-prediction
+
+ai-prediction:
+  type: data
+  title: AI prediction
+
+chart-reviewer:
+  type: manual
+  title: Expert chart reviewer
+  input:
+    - patient-record
+  output:
+    - expert-call
+
+expert-call:
+  type: data
+  title: Expert call/result
+
+comparison:
+  type: finding
+  title: AI vs. expert comparison
+  input:
+    - ai-prediction
+    - expert-call
+`
+
+
 function isObject(o) {
   return o && typeof o === 'object' && !Object.hasOwnProperty(o, 'length');
 }
@@ -307,9 +356,9 @@ class WorkflowEditor extends React.Component {
     super(props);
 
     this.state = {
-      source: props.source || '',
       inputSource: props.source || '',
       inputElems: parseSource(props.source || ''),
+      source: props.source || defaultWorkflow,
       elems: {},
       syntaxError: null,
       validationErrors: null,
